f0bc21b36b5f7c7628f0e6fe44c7df800d6e4e60
Data Floyd <floyddata@gmail.com>
2018-09-20 08:37:16 +0300

    plot_svm_decision_region_2d fonksiyonu eklendi
    Signed-off-by: Data Floyd <floyddata@gmail.com>

--- datafloyd/plot.py
+++ datafloyd/plot.py
@@ -2,11 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Scikit Learn ile kullanılmak üzere hazırlanan karar alanlarını çizen fonksiyon
-def plot_decision_region(X, y, classifier, legend=[], resolution=0.02):
+def plot_decision_region(X, y, classifier, legend=[], resolution=0.02,colors = ('red', 'blue', 'green')):
     # marker ve renk seçimi
     # burada sınıf sayısı kadar modifiye etmek gerekiyor aksi takdirde hata alınır.
     markers = ('o', 'o', 'o')
-    colors = ('red', 'blue', 'green')
+
 
     # karar bölgesi ayarlanıyor
     x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
@@ -31,3 +31,32 @@ def plot_decision_region(X, y, classifier, legend=[], resolution=0.02):
         line_list.append(dummy)
 
     plt.legend(line_list, legend)
+
+# Initially taken from Python Data Science Handbook
+# https://jakevdp.github.io/PythonDataScienceHandbook/05.07-support-vector-machines.html
+def plot_svm_decision_region_2d(model, ax=None, plot_support=True):
+    """Plot the decision function for a 2D SVC"""
+    if ax is None:
+        ax = plt.gca()
+    xlim = ax.get_xlim()
+    ylim = ax.get_ylim()
+
+    # create grid to evaluate model
+    x = np.linspace(xlim[0], xlim[1], 30)
+    y = np.linspace(ylim[0], ylim[1], 30)
+    Y, X = np.meshgrid(y, x)
+    xy = np.vstack([X.ravel(), Y.ravel()]).T
+    P = model.decision_function(xy).reshape(X.shape)
+
+    # plot decision boundary and margins
+    ax.contour(X, Y, P, colors='k',
+               levels=[-1, 0, 1], alpha=0.5,
+               linestyles=['--', '-', '--'])
+
+    # plot support vectors
+    if plot_support:
+        ax.scatter(model.support_vectors_[:, 0],
+                   model.support_vectors_[:, 1],
+                   s=300, linewidth=2, facecolors='none',edgecolor="k",alpha=0.5);
+    ax.set_xlim(xlim)
+    ax.set_ylim(ylim)
